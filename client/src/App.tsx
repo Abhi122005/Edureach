30c0ffd316e610ac335354bf632edc45d6f01887
Abhi122005 <abhishekdivakaran88@gmail.com>
2026-05-06 22:54:01 +0530

    Update project files and ensure environment files remain ignored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,30 @@ import { AuthProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import FloatingChatButton from "./components/FloatingChatButton";
+import Navbar from "./components/Navbar";
 
+const WithNavbar = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Toaster position="top-center" />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={
+            <WithNavbar>
+              <HomePage />
+            </WithNavbar>
+          } />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
+        <FloatingChatButton />
       </AuthProvider>
     </BrowserRouter>
   );
